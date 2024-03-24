b2264bdb3c81f9454a2b7e4cd263d054d9921cb7
Muhammad Haulul Azkiyaa <haululazkiyaa@Muhammads-MacBook-Air.local>
2024-03-24 10:26:23 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302223007/Program.cs
+++ tpmodul6_1302223007/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul6_1302223007;
 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SayaTubeVideo video1 = new SayaTubeVideo(
+            "Tutorial Design By Contract - Muhammad Haulul Azkiyaa"
+            );
+        video1.PrintVideoDetails();
+    }
+}
---
+++ tpmodul6_1302223007/SayaTubeVideo.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+namespace tpmodul6_1302223007
+{
+	public class SayaTubeVideo
+	{
+		private int id;
+		private string title;
+		private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            this.title = title;
+            Random generateId = new Random();
+            this.id = generateId.Next(10000, 99999);
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int num)
+        {
+            this.playCount += num;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("ID : " + id + "\nJudul : " + title + "\nJumlah Pemutaran : " + playCount + "\n");
+        }
+    }
+}
+
